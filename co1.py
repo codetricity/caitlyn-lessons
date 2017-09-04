83061a53404d7d66b47177a7be907768319d0253
Craig <craig@oppkey.com>
2017-09-04 15:36:44 -0700

    added sea creatures

--- co1.py
+++ co1.py
@@ -1,14 +1,18 @@
 import pygame
 pygame.init()
+testmode = False
 
 FPS = 100
 clock = pygame.time.Clock()
 size = (800, 600)
 screen = pygame.display.set_mode(size)
 
+
+
 blue = (127, 255, 212)
 purple = (180, 182, 227)
 pink = (255, 103, 180)
+yellow = (255, 252, 197)
 white = (255, 255, 255)
 
 rightarrow = pygame.image.load('img/arrows/right.png')
@@ -30,14 +34,30 @@ pineapple = pygame.image.load("img/pineapple.png")
 pineapplerect = pygame.Rect(308, 497, 64, 64)
 pineappleon = True
 
+pea = pygame.image.load("img/pea.png")
+pearect = pygame.Rect(650, 80, 64, 64)
+peaon = True
+
 puffer = pygame.image.load("img/puffer.png")
 pufferrect = pygame.Rect(193, 442, 64, 64)
 
-fruitnumstart = 1
+shark = pygame.image.load('img/shark.png')
+sharkrect = pygame.Rect(600, 100, 64, 64)
+
+squid = pygame.image.load('img/squid.png')
+squidrect = pygame.Rect(602, 123, 64, 64)
+
+manta = pygame.image.load('img/manta.png')
+mantarect = pygame.Rect(411, 421, 64, 64)
+
+fruitnumstart = 2
 fruitnum = fruitnumstart
 
 cupcake = pygame.image.load("img/cupcake.png")
 cupcakerect = pygame.Rect(400, 173, 64, 64)
+
+seaweed = pygame.image.load('img/seaweed.png')
+seaweedrect = pygame.Rect(301, 210, 64, 72)
 
 fonts = pygame.font.Font("font/animeace2_ital.ttf", 18)
 playagainsurface = fonts.render("Play Again", False, white)
@@ -46,7 +66,8 @@ playagainrect = playagainsurface.get_rect(left=350, top=375)
 quitsurface = fonts.render("Quit", False, white)
 quitrect = quitsurface.get_rect(left=370, top=425)
 speed = 4
-lspeed = 2
+lspeed = 1
+sspeed = 2
 direction = "up"
 level = 1
 gameon = True
@@ -69,6 +90,8 @@ while gameon:
                 if playagainrect.collidepoint(mousepos):
                     guffyrect.center = (100, 100)
                     leorect.center = (700, 321)
+                    pineappleon = True
+                    peaon = True
                     fruitnum = fruitnumstart
                     level = 1
                 if quitrect.collidepoint(mousepos):
@@ -85,6 +108,8 @@ while gameon:
         screen.blit(cupcake, cupcakerect)
         screen.blit(guffy, guffyrect)
         screen.blit(leo, leorect)
+        
+
 
         if direction == "right":
             guffyrect.centerx = guffyrect.centerx + speed
@@ -104,15 +129,16 @@ while gameon:
         if guffyrect.bottom > 600:
             direction = "up"
 
-        if guffyrect.centerx > leorect.centerx:
-            leorect.centerx = leorect.centerx + lspeed
-        if guffyrect.centerx < leorect.centerx:
-            leorect.centerx = leorect.centerx - lspeed
-        if guffyrect.centery < leorect.centery:
-            leorect.centery = leorect.centery - lspeed
-        if guffyrect.centery > leorect.centery:
-            leorect.centery = leorect.centery + lspeed
-        
+        if not testmode:
+            if guffyrect.centerx > leorect.centerx:
+                leorect.centerx = leorect.centerx + lspeed
+            if guffyrect.centerx < leorect.centerx:
+                leorect.centerx = leorect.centerx - lspeed
+            if guffyrect.centery < leorect.centery:
+                leorect.centery = leorect.centery - lspeed
+            if guffyrect.centery > leorect.centery:
+                leorect.centery = leorect.centery + lspeed
+            
         if pineappleon:
             if guffyrect.colliderect(pineapplerect):
                 speed = 5
@@ -120,6 +146,14 @@ while gameon:
                 fruitnum = fruitnum - 1
         if pineappleon:
             screen.blit(pineapple, pineapplerect)
+
+        if peaon:
+            if guffyrect.colliderect(pearect):
+                speed = 5
+                peaon = False
+                fruitnum = fruitnum - 1
+        if peaon:
+            screen.blit(pea, pearect)
 
         if guffyrect.colliderect(cupcakerect):
             speed = 2
@@ -129,16 +163,59 @@ while gameon:
     
         if guffyrect.colliderect(leorect):
             screen.fill(pink)
-            panda = fonts.render("Game Over", False, white)
+            gameoversurface = fonts.render("Game Over", False, white)
            
-            screen.blit(panda, (350, 302))
+            screen.blit(gameoversurface, (350, 302))
             screen.blit(playagainsurface, playagainrect)
             screen.blit(quitsurface, quitrect)
             level = 100
 
 
     if level == 2:
+        testmode = False
         screen.fill(purple)
         screen.blit(puffer, pufferrect)
+        screen.blit(shark, sharkrect)
+        screen.blit(squid, squidrect)
+        screen.blit(manta, mantarect)
+        screen.blit(seaweed, seaweedrect)
+        screen.blit(rightarrow, rightarrow_rect)
+        screen.blit(larrow, larrow_rect)
+        screen.blit(uarrow, uarrow_rect)
+        screen.blit(darrow, darrow_rect)
+
+        if direction == "right":
+            pufferrect.centerx = pufferrect.centerx + speed
+        if direction == 'left':
+            pufferrect.centerx = pufferrect.centerx - speed
+        if direction == "up":
+            pufferrect.centery = pufferrect.centery - speed
+        if direction == 'down':
+            pufferrect.centery = pufferrect.centery + speed
+
+        if pufferrect.right > 800:
+            direction = "left"
+        if pufferrect.left < 0:
+            direction = "right"
+        if pufferrect.top < 0:
+            direction = "down"
+        if pufferrect.bottom > 600:
+            direction = "up"
+
+        if not testmode:
+            if pufferrect.centerx > sharkrect.centerx:
+                sharkrect.centerx = sharkrect.centerx + sspeed
+            if pufferrect.centerx < sharkrect.centerx:
+                sharkrect.centerx = sharkrect.centerx - sspeed
+            if pufferrect.centery < sharkrect.centery:
+                sharkrect.centery = sharkrect.centery - sspeed
+            if pufferrect.centery > sharkrect.centery:
+                sharkrect.centery = sharkrect.centery + sspeed
+        
+        if pufferrect.colliderect(sharkrect):
+            screen.fill(yellow)
+
+
+
     clock.tick(FPS)
     pygame.display.update()
